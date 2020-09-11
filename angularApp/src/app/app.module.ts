ad269a5c4b219098fb5cbc08f63ab7a1d1a873a2
Usuario <sebastian_felipe1507@hotmail.com>
2020-09-11 19:32:25 -0300

    componentes

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaPersonasComponent } from './tabla-personas/tabla-personas.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TablaPersonasComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
