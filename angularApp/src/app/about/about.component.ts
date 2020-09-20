7aec7f67c6cfba579ff2bf51c86cd60c3bbc341e
Usuario <sebastian_felipe1507@hotmail.com>
2020-09-20 18:13:43 -0300

    spa update

--- angularApp/src/app/about/about.component.ts
+++ angularApp/src/app/about/about.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Post } from '../Post';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  posts: Post[];
+  constructor(private DataService: DataService) {
+    this.DataService.getData().subscribe((data) => {
+      this.posts = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
