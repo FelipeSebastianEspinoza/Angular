3f1fb43946e6f3f4185c4475a72c30dc07bad07e
Usuario <sebastian_felipe1507@hotmail.com>
2020-09-28 21:57:25 -0300

    mini update angular material

--- angularCrud/src/app/components/navigation/navigation.component.ts
+++ angularCrud/src/app/components/navigation/navigation.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ThemePalette} from '@angular/material/core';
+import {ProgressBarMode} from '@angular/material/progress-bar';
+ 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  //Aqui van los valores que se envian al "html" estos son de ejemplo
+  color: ThemePalette = 'primary';
+  mode: ProgressBarMode = 'determinate';
+  value = 50;
+  bufferValue = 75;
+
 
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
 
 }
+ 
